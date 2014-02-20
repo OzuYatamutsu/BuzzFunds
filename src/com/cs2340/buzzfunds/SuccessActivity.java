451340ae5b3116e3d0b37a249dbccb038f55cfbf
Sean Collins <scollins36@gatech.edu>
2014-02-20 12:05:14 -0500

    Added SuccessActivity to transition to instead (as per M4 guidelines)

---
+++ src/com/cs2340/buzzfunds/SuccessActivity.java
@@ -0,0 +1,24 @@
+package com.cs2340.buzzfunds;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class SuccessActivity extends Activity {
+	Intent intent = getIntent();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_success);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.success, menu);
+		return true;
+	}
+
+}
