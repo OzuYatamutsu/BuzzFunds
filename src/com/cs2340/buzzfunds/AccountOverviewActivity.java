33e0d3ff9a6b264141cc7b30818fd942204c14fe
Sean Collins <scollins36@gatech.edu>
2014-02-26 17:43:18 -0500

    Check authentication state of user.

--- src/com/cs2340/buzzfunds/AccountOverviewActivity.java
+++ src/com/cs2340/buzzfunds/AccountOverviewActivity.java
@@ -2,14 +2,22 @@ package com.cs2340.buzzfunds;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class AccountOverviewActivity extends Activity {
-
+	boolean isAuth = getIntent().getExtras().getBoolean("AUTH_STATE");
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_account_overview);
+		if (!isAuth) {
+			// User is not (yet) allowed to be here!
+			Intent toLogin = new Intent(this, LoginActivity.class);
+			toLogin.putExtra("AUTH_ERROR", true);
+			startActivity(toLogin);
+		}
 	}
 
 	@Override
