c8b0c6028214caa33484c03e264db05f58d38250
Sean Collins <scollins36@gatech.edu>
2014-03-07 12:52:49 -0500

    Added JSONMap interface to convert JSON response to <K, V> format (not done)

---
+++ src/com/cs2340/buzzfunds/JSONMap.java
@@ -0,0 +1,89 @@
+package com.cs2340.buzzfunds;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * An object which translates a JSON-encoded String 
+ * into a Map of key-value pairs.
+ * 
+ * @author Sean Collins
+ */
+public class JSONMap implements Map<String, String> {
+	/**
+	 * The key-value map to store data in.
+	 */
+	Map<String, String> map;
+
+	/**
+	 * Constructs a new JSONMap and adds all values to this JSONMap.
+	 * 
+	 * @param jsonString The JSON-encoded String to translate
+	 */
+	public JSONMap(String jsonString) {
+		map = new HashMap<String, String>();
+		// Convert here
+	}
+	
+	@Override
+	public void clear() {
+		map.clear();
+	}
+
+	@Override
+	public boolean containsKey(Object arg0) {
+		return map.containsKey(arg0);
+	}
+
+	@Override
+	public boolean containsValue(Object arg0) {
+		return map.containsValue(arg0);
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<String, String>> entrySet() {
+		return map.entrySet();
+	}
+
+	@Override
+	public String get(Object arg0) {
+		return map.get(arg0);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	@Override
+	public Set<String> keySet() {
+		return map.keySet();
+	}
+
+	@Override
+	public String put(String arg0, String arg1) {
+		return map.put(arg0, arg1);
+	}
+
+	@Override
+	public void putAll(Map<? extends String, ? extends String> arg0) {
+		map.putAll(arg0);
+	}
+
+	@Override
+	public String remove(Object arg0) {
+		return map.remove(arg0);
+	}
+
+	@Override
+	public int size() {
+		return map.size();
+	}
+
+	@Override
+	public Collection<String> values() {
+		return map.values();
+	}
+}
