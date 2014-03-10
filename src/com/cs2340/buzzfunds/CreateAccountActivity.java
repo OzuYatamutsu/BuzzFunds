4208cd608bb865637fd5f63633c89ae6dff06546
Sean Collins <scollins36@gatech.edu>
2014-03-10 10:48:36 -0400

    Added CreateAccountActivity.

---
+++ src/com/cs2340/buzzfunds/CreateAccountActivity.java
@@ -0,0 +1,28 @@
+package com.cs2340.buzzfunds;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CreateAccountActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_account);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.create_account, menu);
+		return true;
+	}
+	
+	public boolean createAccount() {
+		// Create account logic here, use Authenticator
+		// If true, go back to AccountOverviewActivity
+		return false;
+	}
+
+}
