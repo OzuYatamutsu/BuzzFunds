7f5b9e66878789c7e0fbd349681168d7733cb7bb
Sean Collins <scollins36@gatech.edu>
2014-03-10 13:35:03 -0400

    Updated CreateAccountActivity.

--- gen/com/cs2340/buzzfunds/R.java
+++ gen/com/cs2340/buzzfunds/R.java
@@ -31,15 +31,15 @@ public final class R {
         public static final int action_settings=0x7f080017;
         public static final int button2=0x7f080010;
         public static final int createAccountButton=0x7f080001;
+        public static final int create_account_checking=0x7f080007;
         public static final int create_account_newId=0x7f080004;
+        public static final int create_account_savings=0x7f080008;
         public static final int create_account_submit=0x7f080003;
         public static final int login=0x7f08000e;
         public static final int login_form=0x7f08000b;
         public static final int login_status=0x7f080009;
         public static final int login_status_message=0x7f08000a;
         public static final int password=0x7f08000d;
-        public static final int radio0=0x7f080008;
-        public static final int radio1=0x7f080007;
         public static final int radioGroup1=0x7f080005;
         public static final int register_password=0x7f080011;
         public static final int register_username=0x7f080014;
@@ -69,10 +69,10 @@ public final class R {
         public static final int success=0x7f070005;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f05001c;
+        public static final int action_forgot_password=0x7f05001d;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f05001a;
-        public static final int action_sign_in_short=0x7f05001b;
+        public static final int action_sign_in_register=0x7f05001b;
+        public static final int action_sign_in_short=0x7f05001c;
         public static final int add_account=0x7f05000f;
         public static final int app_name=0x7f050000;
         public static final int create_account=0x7f050015;
@@ -80,30 +80,31 @@ public final class R {
         public static final int create_account_id=0x7f050011;
         public static final int create_account_savings=0x7f050013;
         public static final int create_account_type=0x7f050012;
-        public static final int error_field_required=0x7f050021;
-        public static final int error_incorrect_password=0x7f050020;
-        public static final int error_invalid_password=0x7f05001f;
-        public static final int error_invalid_username=0x7f05001e;
+        public static final int error_account_create=0x7f050017;
+        public static final int error_field_required=0x7f050022;
+        public static final int error_incorrect_password=0x7f050021;
+        public static final int error_invalid_password=0x7f050020;
+        public static final int error_invalid_username=0x7f05001f;
         public static final int error_username_exists=0x7f05000a;
         public static final int hello_world=0x7f050002;
         public static final int login_button_text=0x7f050003;
-        public static final int login_progress_signing_in=0x7f05001d;
+        public static final int login_progress_signing_in=0x7f05001e;
         public static final int login_sucess_text=0x7f050004;
         public static final int new_account_button=0x7f050016;
         public static final int no_accounts=0x7f05000e;
-        public static final int prompt_password=0x7f050019;
+        public static final int prompt_password=0x7f05001a;
         /**  Strings related to login 
          */
-        public static final int prompt_username=0x7f050018;
+        public static final int prompt_username=0x7f050019;
         public static final int register_button_text=0x7f050009;
         public static final int register_password_text=0x7f050007;
         public static final int register_passwordverify_text=0x7f050008;
         public static final int register_username_text=0x7f050006;
-        public static final int registerbutton_text=0x7f050022;
+        public static final int registerbutton_text=0x7f050023;
         public static final int success_text=0x7f05000c;
         public static final int title_activity_account_overview=0x7f05000d;
         public static final int title_activity_create_account=0x7f050010;
-        public static final int title_activity_login=0x7f050017;
+        public static final int title_activity_login=0x7f050018;
         public static final int title_activity_register=0x7f050005;
         public static final int title_activity_success=0x7f05000b;
     }
--- src/com/cs2340/buzzfunds/CreateAccountActivity.java
+++ src/com/cs2340/buzzfunds/CreateAccountActivity.java
@@ -2,14 +2,32 @@ package com.cs2340.buzzfunds;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class CreateAccountActivity extends Activity {
 
+	EditText mAccountId;
+	RadioButton mAccountTypeSavings;
+	RadioButton mAccountTypeChecking;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_account);
+		mAccountId = (EditText) findViewById(R.id.create_account_newId);
+		mAccountTypeSavings = (RadioButton) findViewById(R.id.create_account_savings);
+		mAccountTypeChecking = (RadioButton) findViewById(R.id.create_account_checking);
+	}
+	
+	private String getAccountType() {
+		if (mAccountTypeSavings.isChecked()) {
+			return "savings";
+		} else {
+			return "checking";
+		}
 	}
 
 	@Override
@@ -19,10 +37,29 @@ public class CreateAccountActivity extends Activity {
 		return true;
 	}
 	
-	public boolean createAccount() {
-		// Create account logic here, use Authenticator
-		// If true, go back to AccountOverviewActivity
-		return false;
+	public void createAccount() {
+		if (submitAccount()) {
+			// Successful, transition back to AccountOverviewActivity
+			Intent intent = new Intent(this, AccountOverviewActivity.class);
+			startActivity(intent);
+		} else {
+			mAccountId.setError(getString(R.string.error_account_create));
+		}
+	}
+	
+	public boolean submitAccount() {
+		boolean result = false;
+		if (IntentSingleton.getString("USERNAME") != null) {
+			Authenticator auth = new Authenticator(DefaultConnection.BUZZFUNDS, 
+					IntentSingleton.getString("USERNAME"));
+			Account newAccount = auth.httpGetAddAccount(mAccountId.getText().toString(), 
+					getAccountType());
+			if (newAccount != null) {
+				result = true;
+			}
+		}
+		
+		return result;
 	}
 
 }
