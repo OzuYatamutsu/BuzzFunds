78a5868756dd762fbadec4cd511db4e48ca39f4f
Sean Collins <scollins36@gatech.edu>
2014-03-11 10:47:49 -0400

    Added withdrawal Transaction logic (untested, crashes on JSON response)

--- src/com/cs2340/buzzfunds/WithdrawalTransactionActivity.java
+++ src/com/cs2340/buzzfunds/WithdrawalTransactionActivity.java
@@ -2,15 +2,32 @@ package com.cs2340.buzzfunds;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class WithdrawalTransactionActivity extends Activity {
-
+	Account account;
+	EditText mAmount, mYear, mMonth, mDay;
+	Intent intent;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Setup.ignoreMainNetworkException();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_withdrawal_transaction);
+		mAmount = (EditText) findViewById(R.id.amount);
+		mYear = (EditText) findViewById(R.id.year);
+		mMonth = (EditText) findViewById(R.id.month);
+		mDay = (EditText) findViewById(R.id.day);
+		
+		if (IntentSingleton.keyExists("CURRENT_ACCOUNT")) {
+			account = IntentSingleton.getAccount("CURRENT_ACCOUNT");
+		} else {
+			intent = new Intent(this, AccountOverviewActivity.class);
+			startActivity(intent);
+		}
 	}
 
 	@Override
@@ -18,6 +35,46 @@ public class WithdrawalTransactionActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.withdrawal_transaction, menu);
 		return true;
+	}
+	
+	public void attemptTransaction(View view) {
+		String date = mYear.getText() + "-" + mMonth.getText() + "-" + mDay.getText();
+		
+		if (queueTransaction(date)) {
+			if (pushTransaction(date)) {
+				// All good!
+				Intent intent = new Intent(this, AccountDetailActivity.class);
+				startActivity(intent);
+			} else {
+				mAmount.setError(getString(R.string.error_transaction));
+			}
+		} else {
+			mAmount.setError(getString(R.string.error_transaction));
+			
+		}
+		
+		
+	}
+	
+	private boolean queueTransaction(String date) {
+		boolean result = true;
+		
+		double amount = Double.parseDouble(mAmount.getText().toString());
+		Transaction transaction = new Transaction(account, amount, date, "withdrawal");
+		// Title is just date for now
+		account.queue(transaction);
+		
+		return result;
+	}
+	
+	private boolean pushTransaction(String date) {
+		boolean result = false;
+		
+		if (account.pushNeeded()) {
+			account.push(date);
+		}
+		
+		return result;
 	}
 
 }
