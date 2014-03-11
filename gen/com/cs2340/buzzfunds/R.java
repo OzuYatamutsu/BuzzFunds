26e93da4c60b68da461740acf3ec0ef8b931e5dc
Sean Collins <scollins36@gatech.edu>
2014-03-10 22:33:47 -0400

    Added choosing type of Transaction logic to ChooseTransactionTypeActivity.

--- gen/com/cs2340/buzzfunds/R.java
+++ gen/com/cs2340/buzzfunds/R.java
@@ -24,35 +24,39 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int TextView01=0x7f080019;
-        public static final int TextView02=0x7f080016;
+        public static final int TextView01=0x7f08001d;
+        public static final int TextView02=0x7f08001a;
         public static final int accountList=0x7f080006;
         public static final int account_detail_name=0x7f080000;
-        public static final int action_forgot_password=0x7f08001c;
-        public static final int action_settings=0x7f08001b;
-        public static final int button2=0x7f080014;
+        public static final int action_forgot_password=0x7f080020;
+        public static final int action_settings=0x7f08001f;
+        public static final int button2=0x7f080018;
         public static final int createAccountButton=0x7f080007;
-        public static final int create_account_checking=0x7f08000c;
-        public static final int create_account_newId=0x7f080009;
-        public static final int create_account_savings=0x7f08000b;
+        public static final int create_account_checking=0x7f080010;
+        public static final int create_account_newId=0x7f08000d;
+        public static final int create_account_savings=0x7f08000f;
         public static final int create_account_submit=0x7f080008;
         public static final int detail_balance_label=0x7f080001;
         public static final int detail_balance_val=0x7f080003;
         public static final int detail_start_transaction=0x7f080005;
         public static final int detail_type_label=0x7f080002;
         public static final int detail_type_val=0x7f080004;
-        public static final int login=0x7f080012;
-        public static final int login_form=0x7f08000f;
-        public static final int login_status=0x7f08000d;
-        public static final int login_status_message=0x7f08000e;
-        public static final int newAccountType=0x7f08000a;
-        public static final int password=0x7f080011;
-        public static final int register_password=0x7f080015;
-        public static final int register_username=0x7f080018;
-        public static final int register_verify=0x7f080017;
-        public static final int registerbutton=0x7f08001a;
-        public static final int sign_in_button=0x7f080013;
-        public static final int username=0x7f080010;
+        public static final int login=0x7f080016;
+        public static final int login_form=0x7f080013;
+        public static final int login_status=0x7f080011;
+        public static final int login_status_message=0x7f080012;
+        public static final int newAccountType=0x7f08000e;
+        public static final int password=0x7f080015;
+        public static final int register_password=0x7f080019;
+        public static final int register_username=0x7f08001c;
+        public static final int register_verify=0x7f08001b;
+        public static final int registerbutton=0x7f08001e;
+        public static final int sign_in_button=0x7f080017;
+        public static final int transaction_choose_text=0x7f08000c;
+        public static final int transaction_deposit_button=0x7f08000a;
+        public static final int transaction_transfer_button=0x7f08000b;
+        public static final int transaction_withdrawal_button=0x7f080009;
+        public static final int username=0x7f080014;
     }
     public static final class layout {
         public static final int activity_account_detail=0x7f030000;
@@ -84,10 +88,10 @@ public final class R {
     }
     public static final class string {
         public static final int account_detail_name=0x7f050018;
-        public static final int action_forgot_password=0x7f05002a;
+        public static final int action_forgot_password=0x7f05002e;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f050028;
-        public static final int action_sign_in_short=0x7f050029;
+        public static final int action_sign_in_register=0x7f05002c;
+        public static final int action_sign_in_short=0x7f05002d;
         public static final int add_account=0x7f05000f;
         public static final int app_name=0x7f050000;
         public static final int create_account=0x7f050015;
@@ -101,39 +105,43 @@ public final class R {
         public static final int detail_type_label=0x7f050019;
         public static final int detail_type_val=0x7f05001c;
         public static final int error_account_create=0x7f050017;
-        public static final int error_field_required=0x7f05002f;
-        public static final int error_incorrect_password=0x7f05002e;
-        public static final int error_invalid_password=0x7f05002d;
-        public static final int error_invalid_username=0x7f05002c;
+        public static final int error_field_required=0x7f050033;
+        public static final int error_incorrect_password=0x7f050032;
+        public static final int error_invalid_password=0x7f050031;
+        public static final int error_invalid_username=0x7f050030;
         public static final int error_username_exists=0x7f05000a;
         public static final int hello_world=0x7f050002;
         public static final int login_button_text=0x7f050003;
-        public static final int login_progress_signing_in=0x7f05002b;
+        public static final int login_progress_signing_in=0x7f05002f;
         public static final int login_sucess_text=0x7f050004;
         public static final int new_account_button=0x7f050016;
         public static final int no_accounts=0x7f05000e;
-        public static final int prompt_password=0x7f050027;
+        public static final int prompt_password=0x7f05002b;
         /**  Strings related to login 
          */
-        public static final int prompt_username=0x7f050026;
+        public static final int prompt_username=0x7f05002a;
         public static final int register_button_text=0x7f050009;
         public static final int register_password_text=0x7f050007;
         public static final int register_passwordverify_text=0x7f050008;
         public static final int register_username_text=0x7f050006;
-        public static final int registerbutton_text=0x7f050030;
+        public static final int registerbutton_text=0x7f050034;
         public static final int success_text=0x7f05000c;
         public static final int title_activity_account_detail=0x7f05001e;
         public static final int title_activity_account_overview=0x7f05000d;
         public static final int title_activity_choose_transaction_type=0x7f050020;
         public static final int title_activity_create_account=0x7f050010;
         public static final int title_activity_deposit_transaction=0x7f050021;
-        public static final int title_activity_login=0x7f050025;
+        public static final int title_activity_login=0x7f050029;
         public static final int title_activity_register=0x7f050005;
         public static final int title_activity_success=0x7f05000b;
         public static final int title_activity_transaction_create=0x7f05001f;
         public static final int title_activity_transfer_transaction=0x7f050023;
         public static final int title_activity_withdrawal_transaction=0x7f050024;
         public static final int title_activity_withdrawl_transaction=0x7f050022;
+        public static final int transaction_choose_text=0x7f050028;
+        public static final int transaction_deposit_button=0x7f050025;
+        public static final int transaction_transfer_button=0x7f050027;
+        public static final int transaction_withdrawal_button=0x7f050026;
     }
     public static final class style {
         /** 
