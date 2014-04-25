826d4478ccd29ff12b9dc01944e5638be61a3599
Sean Collins <scollins36@gatech.edu>
2014-04-24 22:29:46 -0400

    Merge commit

--- gen/com/cs2340/buzzfunds/R.java
+++ gen/com/cs2340/buzzfunds/R.java
@@ -118,16 +118,15 @@ public final class R {
         public static final int choose_transaction_type=0x7f080002;
         public static final int create_account=0x7f080003;
         public static final int deposit_transaction=0x7f080004;
-        public static final int graph=0x7f080005;
-        public static final int history=0x7f080006;
-        public static final int login=0x7f080007;
-        public static final int main=0x7f080008;
-        public static final int register=0x7f080009;
-        public static final int splash=0x7f08000a;
-        public static final int success=0x7f08000b;
-        public static final int transaction=0x7f08000c;
-        public static final int transfer_transaction=0x7f08000d;
-        public static final int withdrawal_transaction=0x7f08000e;
+        public static final int history=0x7f080005;
+        public static final int login=0x7f080006;
+        public static final int main=0x7f080007;
+        public static final int register=0x7f080008;
+        public static final int splash=0x7f080009;
+        public static final int success=0x7f08000a;
+        public static final int transaction=0x7f08000b;
+        public static final int transfer_transaction=0x7f08000c;
+        public static final int withdrawal_transaction=0x7f08000d;
     }
     public static final class raw {
         public static final int click=0x7f040000;
